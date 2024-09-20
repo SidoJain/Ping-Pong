06c0e8d07d593f94427abf1d8eb756aeb3b4f272
Siddharth Jain <sidojain30705@gmail.com>
2024-09-20 18:04:42 +0530

    updated gitignore

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sidoj\Desktop\Sido\Coding\Python\Projects\Ping Pong\myenv
+set VIRTUAL_ENV=C:\Users\sidoj\Desktop\Coding\Python\Projects\Ping Pong\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
